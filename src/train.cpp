8c4dc753ac5d08d5e81ed0b2cc38fc703a085c86
Decimator003 <104575851+Decimator003@users.noreply.github.com>
2024-11-27 23:39:18 +0530

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -1,4 +1,6 @@
 #include "../include/train.h"
 
-Train::Train(int id, int priority, std::string entry, std::string exit)
-    : train_id(id), priority(priority), entry_point(entry), exit_point(exit) {}
+Train::Train(int id, int priority, std::string entry, std::string exit,
+             std::string arrival, std::string departure)
+    : train_id(id), priority(priority), entry_point(entry), exit_point(exit),
+      arrival_time(arrival), departure_time(departure) {}
